dbaca7ab2279f37934d47d90160b0467dfaf0679
Kolier <kolier.li@gmail.com>
2014-12-14 02:06:51 +0800

    === 7.x-0.1 === Init: Basic function is working.

---
+++ admin_menu_toggle.js
@@ -0,0 +1,19 @@
+(function($) {
+  // Initiate if not exist yet.
+  Drupal.admin = Drupal.admin || {};
+  Drupal.admin.behaviors = Drupal.admin.behaviors || {};
+
+  Drupal.admin.behaviors.toggleShowHide = function (context, settings, $adminMenu) {
+    $("#admin-menu-toggle").click(function () {
+      if ($(this).hasClass('toggle-show')) {
+        $(this).removeClass("toggle-show").addClass("toggle-hide");
+        $("#admin-menu-wrapper").removeClass("element-hidden");
+      }
+      else if ($(this).hasClass('toggle-hide')) {
+        $(this).removeClass("toggle-hide").addClass("toggle-show");
+        $("#admin-menu-wrapper").addClass("element-hidden");
+      }
+    });
+  }
+
+})(jQuery);
